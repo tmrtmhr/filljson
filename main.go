4edebda2f04bffa244a96f7a67b8424ccaca7f13
TAMURA Tomohiro <tamura@goga.co.jp>
2017-03-14 21:23:21 +0900

    Refactoring for UT

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
   "strconv"
 )
 
+type Json map[string]interface{}
+
 func main() {
   flag.Parse()
   jsonStr, err := ioutil.ReadAll(os.Stdin)
@@ -26,27 +28,40 @@ func main() {
   if err != nil {
     fmt.Println("ERROR: ", err)
   }
-  dataStr := string(data)
 
-  var value interface{}
-  switch valueType {
-  case "[string]": value = strings.Split(strings.TrimSpace(dataStr), "\n")
-  case "float": {
-    value,err = strconv.ParseFloat(strings.TrimSpace(dataStr), 64)
-    if err != nil { fmt.Println("ERROR: ", err) }
-  }
-  case "int": {
-    value,err = strconv.ParseInt(strings.TrimSpace(dataStr), 10, 0)
-    if err != nil { fmt.Println("ERROR: ", err) }
-  }
-  case "string": fallthrough
-  default: value = dataStr
-  }
+  value := Parse(valueType, string(data))
 
   var jsonData map[string]interface{}
   _ = json.Unmarshal(jsonStr, &jsonData)
 
-  finger := jsonData
+  jsonData = Fill(jsonData,propNames,value)
+
+  str, err := json.Marshal(jsonData)
+  fmt.Print(string(str))
+}
+
+func Parse(valueType string, valueStr string) interface{} {
+  var value interface{}
+  var err error
+  switch valueType {
+  case "[string]": value = strings.Split(strings.TrimSpace(valueStr), "\n")
+  case "float": {
+    value,err = strconv.ParseFloat(strings.TrimSpace(valueStr), 64)
+    if err != nil { fmt.Println("ERROR: ", err) }
+  }
+  case "int": {
+    value,err = strconv.ParseInt(strings.TrimSpace(valueStr), 10, 0)
+    if err != nil { fmt.Println("ERROR: ", err) }
+  }
+  case "string": fallthrough
+  default: value = valueStr
+  }
+  return value
+}
+
+
+func Fill(json Json, propNames []string, value interface{}) Json {
+  finger := json
   for idx, propName := range propNames {
     if (idx == len(propNames) -1) {
       finger[propName] = value
@@ -54,7 +69,5 @@ func main() {
       finger = finger[propName].(map[string]interface{})
     }
   }
-
-  str, err := json.Marshal(jsonData)
-  fmt.Print(string(str))
+  return json
 }
